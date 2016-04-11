b1d50555a15c1f5bc30e0a9276a68537d6dcc7a3
Andrew Molchanov <mr.catmanrus@gmail.com>
2016-04-12 00:41:48 +0300

    fix #1

--- js/jcplayer.js
+++ js/jcplayer.js
@@ -4,8 +4,7 @@
  https://github.com/JoCat/JCPlayer
 */
 $(document).ready(function(){
-    $("#jcp-player").html('<i id="jcp-play" class="jcp-play"></i><i id="jcp-pause" class="jcp-pause" style="display:none;"></i><i id="jcp-stop" class="jcp-stop"></i><i id="jcp-reload" class="jcp-reload"></i><i id="jcp-volume-off" class="jcp-volume-off" style="display:none;"></i><i id="jcp-volume-down" class="jcp-volume-down"></i><input id="jcp-volume" type="range" min="0" max="10" value="5" step="0.1" /><i id="jcp-volume-up" class="jcp-volume-up"></i><span id="jcp-duration" class="jcp-duration"></span><span id="jcp-track"></span>');
-    $("#jcp-player").attr('class','jcp-player');
+    $("#jcp-player").html('<i id="jcp-play"></i><i id="jcp-pause" style="display:none;"></i><i id="jcp-stop"></i><i id="jcp-reload"></i><i id="jcp-volume-off" style="display:none;"></i><i id="jcp-volume-down"></i><input id="jcp-volume" type="range" min="0" max="10" value="5" step="0.1" /><i id="jcp-volume-up"></i><span id="jcp-duration"></span><span id="jcp-track"></span>');
     audio = new Audio();
     playReload = true;
 
@@ -80,17 +79,25 @@ $(document).ready(function(){
         else {$("#jcp-duration").html(m+':'+s)}
     },1000);
 
-    timer = setTimeout(function showinfo()	{
+    timer = setTimeout(function showinfo(){
         $.ajax({
           dataType: 'jsonp',
           jsonp: 'callback',
           jsonpCallback: 'info',
           url: info,
-          success: function(data){
-            if (data.live.title == '')//First Mount (Main)
-                $('#jcp-track').html(data.nonstop.title);//Second Mount (Fallback)
+          success: function(d){
+            try {
+                if (d.live.title == '') {einfo = true} else einfo = false;//First Mount (Main)
+            } catch(e) {
+                einfo = true;
+            }
+            if (einfo == true)
+                $('#jcp-track').html(d.nonstop.title);//Second Mount (Fallback)
             else
-                $('#jcp-track').html(data.live.title);//First Mount (Main)
+                $('#jcp-track').html(d.live.title);//First Mount (Main)
+          },
+          error: function(){
+              $("#jcp-track").html('Ошибка загрузки! Возможно радио сейчас не работает...');
           }
         });
         timer = setTimeout(showinfo,tupd*1000);
