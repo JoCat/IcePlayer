271226d5ca6199a6fbabe6c9afa2cbe258fbbb38
Andrew Molchanov <mr.catmanrus@gmail.com>
2016-08-10 20:10:21 +0300

    1.2

--- js/jcplayer.js
+++ js/jcplayer.js
@@ -1,35 +1,33 @@
 /*
- JCat Online Player v.1.0 - Player for Site (Icecast2 Online Radio)
+ JCat Online Player v.1.2 - Player for Site (Icecast2 Online Radio)
  Copyright (c) 2016 Andrew Molchanov
  https://github.com/JoCat/JCPlayer
 */
 $(document).ready(function(){
-    $("#jcp-player").html('<i id="jcp-play"></i><i id="jcp-pause" style="display:none;"></i><i id="jcp-stop"></i><i id="jcp-reload"></i><i id="jcp-volume-off" style="display:none;"></i><i id="jcp-volume-down"></i><input id="jcp-volume" type="range" min="0" max="10" value="5" step="0.1" /><i id="jcp-volume-up"></i><span id="jcp-duration"></span><span id="jcp-track"></span>');
-    audio = new Audio();
-    playReload = true;
+    $("#jcp-player").html('<i id="jcp-play"></i><i id="jcp-stop"></i><i id="jcp-reload"></i><i id="jcp-volume-down"></i><input id="jcp-volume" type="range" min="0" max="10" value="5" step="0.1" /><i id="jcp-volume-up"></i><span id="jcp-duration"></span><span id="jcp-track"></span>');
+    var audio = new Audio();
+    var playReload = true;
+    audio.volume = 0.5;
 
-    $("#jcp-play").click(function(){
+    $("#jcp-player").on('click', '#jcp-play', function() {
         if (playReload == true) {
             audio.setAttribute("src",src);
             playReload = false;
         }
         audio.play();
-        $("#jcp-play").css('display','none');
-        $("#jcp-pause").css('display','inline-block');
+        $(this).attr('id', 'jcp-pause');
     });
 
-    $("#jcp-pause").click(function(){
+    $("#jcp-player").on('click', '#jcp-pause', function() {
         audio.pause();
-        $("#jcp-play").css('display','inline-block');
-        $("#jcp-pause").css('display','none');
+        $(this).attr('id', 'jcp-play');
     });
 
     $("#jcp-stop").click(function(){
         audio.pause();
         audio.setAttribute("src","");
         playReload = true;
-        $("#jcp-play").css('display','inline-block');
-        $("#jcp-pause").css('display','none');
+        $('#jcp-pause').attr('id', 'jcp-play');
     });
 
     $("#jcp-reload").click(function(){
@@ -37,42 +35,39 @@ $(document).ready(function(){
         audio.pause();
         audio.setAttribute("src",src);
         audio.play();
-        $("#jcp-play").css('display','none');
-        $("#jcp-pause").css('display','inline-block');
+        $("#jcp-play").attr('id', 'jcp-pause');
     });
 
     $("#jcp-volume").mousemove(function(){
         audio.volume = parseFloat(this.value / 10);
      });
+    $("#jcp-volume").click(function(){
+        $("#jcp-volume-off").attr('id', 'jcp-volume-down');
+    });
 
-    $("#jcp-volume-off").click(function(){
+    $("#jcp-player").on('click', '#jcp-volume-off', function() {
         audio.volume = oldVolume;
-        $("#jcp-volume-off").css('display','none');
-        $("#jcp-volume-down").css('display','inline-block');
+        $(this).attr('id', 'jcp-volume-down');
         $("#jcp-volume").val(oldVolume * 10);
     });
 
-    $("#jcp-volume-down").click(function(){
+    $("#jcp-player").on('click', '#jcp-volume-down', function() {
         oldVolume = audio.volume;
         audio.volume = 0;
-        $("#jcp-volume-down").css('display','none');
-        $("#jcp-volume-off").css('display','inline-block');
+        $(this).attr('id', 'jcp-volume-off');
         $("#jcp-volume").val(0);
     });
 
     $("#jcp-volume-up").click(function(){
         audio.volume = 1;
-        $("#jcp-volume-off").css('display','none');
-        $("#jcp-volume-down").css('display','inline-block');
+        $("#jcp-volume-off").attr('id', 'jcp-volume-down');
         $("#jcp-volume").val(10);
     });
 
     setInterval(function ()	{
-        s = parseInt(audio.currentTime % 60);
-        m = parseInt((audio.currentTime / 60) % 60);
-        h = parseInt((audio.currentTime / 60)/ 60);
-        s = String(s);
-        m = String(m);
+        s = String(parseInt(audio.currentTime % 60));
+        m = String(parseInt((audio.currentTime / 60) % 60));
+        h = parseInt(audio.currentTime / 3600);
         if (s.length <= 1) {s = '0'+s};
         if (m.length <= 1) {m = '0'+m};
         if (h >= 1) {$("#jcp-duration").html(h+':'+m+':'+s)}
