aaf6859edf592756291686c6a6b2e925c9f70d51
Andrew Molchanov <mr.catmanrus@gmail.com>
2019-11-03 08:28:02 +0300

    beta2

--- js/jcplayer.js
+++ js/jcplayer.js
@@ -1,5 +1,5 @@
 /*
- JCat Online Player v.2.0.0_beta1 - Player for Site (Icecast2 Online Radio)
+ JCat Online Player v.2.0.0_beta2 - Player for Site (Icecast2 Online Radio)
  Copyright (c) 2016-2019 Andrew Molchanov
  https://github.com/JoCat/JCPlayer
 */
@@ -19,64 +19,64 @@ JCPlayer = {
     player_stoped: true,
 
     // Functions
-    init: function (init_params) {
+    init(init_params) {
         // Setting transmitted parameters
         if (typeof init_params == 'object') {
             init_params_list = Object.keys(init_params);
             for (let parameter of init_params_list) {
-                JCPlayer[parameter] = init_params[parameter];
+                this[parameter] = init_params[parameter];
             }
         }
 
-        $("#jcp-player").html('<div id="jcp-player" class="'+JCPlayer.style+'"><div id="player-el"><i id="jcp-play"></i><i id="jcp-stop"></i><input id="jcp-volume" type="range" min="0" max="100" value="50" step="1"></div><span id="jcp-track"></span></div>');
+        $("#jcp-player").html('<div id="jcp-player" class="'+this.style+'"><div id="player-el"><i id="jcp-play"></i><i id="jcp-stop"></i><input id="jcp-volume" type="range" min="0" max="100" value="50" step="1"></div><span id="jcp-track"></span></div>');
 
         // Init audio object
-        JCPlayer.audio_object = new Audio();
-        JCPlayer.audio_object.volume = 0.5;
+        this.audio_object = new Audio();
+        this.audio_object.volume = 0.5;
 
         // Events
-        $("#jcp-player").on('click', '#jcp-play', JCPlayer.play);
-        $("#jcp-player").on('click', '#jcp-pause', JCPlayer.pause);
-        $("#jcp-player").on('click', '#jcp-stop', JCPlayer.stop);
-        $("#jcp-player").on('mousemove', '#jcp-volume', JCPlayer.change_volume);
+        $("#jcp-player").on('click', '#jcp-play', (e) => {this.play(e.target)});
+        $("#jcp-player").on('click', '#jcp-pause', (e) => {this.pause(e.target)});
+        $("#jcp-player").on('click', '#jcp-stop', (e) => {this.stop(e.target)});
+        $("#jcp-player").on('mousemove', '#jcp-volume', (e) => {this.change_volume(e.target)});
 
-        timer = setTimeout(function showinfo() {
+        // show current playable track
+        let _this = this;
+        let timer = setTimeout(function showinfo() {
             $.ajax({
                 dataType: 'json',
-                url: JCPlayer.server_address + JCPlayer.info_link,
-                success: function(d) {
-                    for (let mount_name of JCPlayer.mounts_list) {
-                        if (d[mount_name]) {
-                            $("#jcp-track").html(d[mount_name].title);
-                            break;
-                        }
+                url: _this.server_address + _this.info_link,
+            }).done((d) => {
+                for (let mount_name of _this.mounts_list) {
+                    if (d[mount_name]) {
+                        $("#jcp-track").html(d[mount_name].title);
+                        break;
                     }
                 }
             });
-            timer = setTimeout(showinfo,JCPlayer.time_update*1000);
-        },JCPlayer.time_update*1000);
+            timer = setTimeout(showinfo, _this.time_update*1000);
+        }, _this.time_update*1000);
     },
 
-    play: function() {
-        if (JCPlayer.player_stoped === true) {
-            JCPlayer.audio_object.setAttribute("src", JCPlayer.server_address + JCPlayer.stream_mount);
-            JCPlayer.player_stoped = false;
+    play(el) {
+        if (this.player_stoped === true) {
+            this.audio_object.setAttribute("src", this.server_address + this.stream_mount);
+            this.player_stoped = false;
         }
-        JCPlayer.audio_object.play();
-        $(this).attr('id', 'jcp-pause');
+        this.audio_object.play();
+        $(el).attr('id', 'jcp-pause');
     },
-    pause: function() {
-        JCPlayer.audio_object.pause();
-        $(this).attr('id', 'jcp-play');
+    pause(el) {
+        this.audio_object.pause();
+        $(el).attr('id', 'jcp-play');
     },
-    stop: function() {
-        JCPlayer.audio_object.pause();
-        JCPlayer.audio_object.setAttribute("src", "");
-        JCPlayer.player_stoped = true;
-        $('#jcp-pause').attr('id', 'jcp-play');
+    stop(el) {
+        this.audio_object.pause();
+        this.audio_object.setAttribute("src", "");
+        this.player_stoped = true;
+        $(el).attr('id', 'jcp-play');
     },
-    change_volume: function() {
-        JCPlayer.audio_object.volume = parseFloat(this.value / 100);
-    },
-
+    change_volume(el) {
+        this.audio_object.volume = el.value / 100;
+    }
 };
